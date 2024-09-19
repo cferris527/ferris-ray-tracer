c1d6382905c66726130b5dedea6a33f79de3e2d5
Connor Ferris <connorferris52703@gmail.com>
2024-09-19 16:16:13 -0500

    sphere, interval, hittable, hittable list

--- main.cpp
+++ main.cpp
@@ -1,24 +1,16 @@
-#include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "rtweekend.h"
 
-#include <iostream>
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
-}
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r, interval(0, infinity), rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
+    }
 
-color ray_color(const ray& r) {
-    // Red sphere
-    if (hit_sphere(point3(0,0,-1), 0.5, r))
-        return color(1, 0, 0);
-    
-    // This makes the blue gradient
+    // blue gradient
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
@@ -26,13 +18,12 @@ color ray_color(const ray& r) {
 
 /*
     To run:
-    g++ main.cpp -o example
+    g++ main.cpp -Wall -o example
     example > image.ppm (This writes to image.ppm vs directly to console)
 */
 int main() {
 
     // Image
-
     auto aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
 
@@ -40,8 +31,13 @@ int main() {
     int image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
 
-    // Camera
+    // World
+    hittable_list world;
 
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+
+    // Camera
     auto focal_length = 1.0;
     auto viewport_height = 2.0;
     auto viewport_width = viewport_height * (double(image_width)/image_height);
@@ -71,7 +67,7 @@ int main() {
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
