3d4bdaee2bbd7ae9d0d0ce1173db4a5094ff6d4d
Connor Ferris <connorferris52703@gmail.com>
2024-09-30 23:59:36 -0500

    reflective metals, material.h class

--- camera.h
+++ camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H
 
 #include "hittable.h"
+#include "material.h"
 
 class camera {
   public:
@@ -28,7 +29,7 @@ class camera {
             }
         }
 
-        std::clog << "\rRender complete.                 \n";
+        std::clog << "\rRender complete, master Ferris.                 \n";
     }
 
   private:
@@ -94,8 +95,11 @@ class camera {
         hit_record rec;
 
         if (world.hit(r, interval(0.001, infinity), rec)) {
-            vec3 direction = rec.normal + random_unit_vector();
-           return 0.1 * ray_color(ray(rec.p, direction), depth-1, world);
+            ray scattered;
+            color attenuation;
+            if (rec.mat->scatter(r, rec, attenuation, scattered))
+                return attenuation * ray_color(scattered, depth-1, world);
+            return color(0,0,0);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
